8bbd59b2177435e0c891ffca347a2a61e6516a33
Gawain Lynch <gawain.lynch@gmail.com>
2016-03-10 15:09:46 +0100

    Audit extension class loader

---
+++ src/AuditExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Audit;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * Audit extension class loader.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class AuditExtension extends SimpleExtension
+{
+}
