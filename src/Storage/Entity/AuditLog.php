8b27c6105538cf042df19fe3c0c6189cb0917385
Gawain Lynch <gawain.lynch@gmail.com>
2016-04-29 10:21:29 +0200

    AuditLog entity

---
+++ src/Storage/Entity/AuditLog.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Bolt\Extension\Bolt\Audit\Storage\Entity;
+
+use Bolt\Storage\Entity\Entity;
+
+/**
+ * AuditLog entity class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class AuditLog extends Entity
+{
+    /** @var string */
+    protected $event;
+    /** @var integer */
+    protected $reason;
+    /** @var \DateTime */
+    protected $datetime;
+    /** @var string */
+    protected $username;
+    /** @var string */
+    protected $ip;
+    /** @var string */
+    protected $uri;
+    /** @var string */
+    protected $message;
+
+    /**
+     * @return string
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * @param string $event
+     */
+    public function setEvent($event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * @return int
+     */
+    public function getReason()
+    {
+        return $this->reason;
+    }
+
+    /**
+     * @param int $reason
+     */
+    public function setReason($reason)
+    {
+        $this->reason = $reason;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDatetime()
+    {
+        return $this->datetime;
+    }
+
+    /**
+     * @param \DateTime $datetime
+     */
+    public function setDatetime($datetime)
+    {
+        $this->datetime = $datetime;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @param string $ip
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @param string $uri
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+}
